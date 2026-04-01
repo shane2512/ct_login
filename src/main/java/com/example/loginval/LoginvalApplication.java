0e95e10dd833442d24aaaaf36dd644114f5544bd
shane2512 <shanejoans.27csb@licet.ac.in>
2026-04-01 08:42:40 +0530

    login init

---
+++ src/main/java/com/example/loginval/LoginvalApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginvalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loginval/LoginvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
